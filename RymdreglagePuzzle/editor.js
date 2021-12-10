0f57d3accaa56ddec925a29ccede9f59ad425ef7
SuperFloch <fierdehache25@gmail.com>
2021-12-10 17:25:24 +0100

    Ajout lecture auto SnakeCode, chiffre et soulignage

--- RymdreglagePuzzle/editor.js
+++ RymdreglagePuzzle/editor.js
@@ -16,6 +16,7 @@ window.onload = function() {
 	buildGridCodeTable();
 	
 	playlistSelect.onchange = function(e){
+		currentPlaylist = this.value;
 		readFile(this.value,buildForm);
 	};
 	document.getElementById("nextButton").onclick = function(){
@@ -72,6 +73,10 @@ function buildForm(playlistInfo){
 	
 	var idString = fillWithZeros(playlistSelect.value,4);
 	
+	buildGridCodeTable(playlistInfo.img.grid);
+	
+	var black = "#000000";
+	
 	var miniature = document.getElementById("miniaturePlaylist");
 	miniature.src = "./Miniatures/"+idString+".jpg";
 	miniature.onload = function(){
@@ -100,16 +105,158 @@ function buildForm(playlistInfo){
 		}
 		
 		// Grille
-		p = ctx.getImageData(340, 340, 1, 1).data; 
+		p = ctx.getImageData(340, 336, 1, 1).data;
 		var gridColor = "#" + ("000000" + rgbToHex(p[0], p[1], p[2])).slice(-6);
 		
 		document.getElementById("gridColor").value = gridColor;
 		
+		var gridCornerX = 338;
+		var gridCornerY = 335;
+		var gridWidth = 50;
+		var cellWidth = gridWidth/4;
+		for(var y =0;y<4;y++){
+			for(var x =0;x<4;x++){
+				var cellCenterX = gridCornerX+cellWidth/2 + cellWidth * x+x;
+				var cellCenterY = gridCornerY+cellWidth/2 + cellWidth * y+y;
+				
+				var theP = ctx.getImageData(cellCenterX,cellCenterY, 1, 1).data;
+				var cellColor = "#" + ("000000" + rgbToHex(theP[0], theP[1], theP[2])).slice(-6);
+				
+				var isMiddleBlack = cellColor == black;
+				
+				
+				ctx.fillStyle = "white";
+				ctx.fillRect(cellCenterX,cellCenterY,1,1);
+				
+				//console.log(cellColor);
+				if(isMiddleBlack){
+					// North
+					var northPoint = ctx.getImageData(cellCenterX, cellCenterY-cellWidth/2+2, 1, 1).data;
+					var northColor = "#" + ("000000" + rgbToHex(northPoint[0], northPoint[1], northPoint[2])).slice(-6);
+					
+					//South
+					var southPoint = ctx.getImageData(cellCenterX, cellCenterY+cellWidth/2-1, 1, 1).data;
+					var southColor = "#" + ("000000" + rgbToHex(southPoint[0], southPoint[1], southPoint[2])).slice(-6);
+					
+					//East
+					var eastPoint = ctx.getImageData(cellCenterX+cellWidth/2-2, cellCenterY, 1, 1).data;
+					var eastColor = "#" + ("000000" + rgbToHex(eastPoint[0], eastPoint[1], eastPoint[2])).slice(-6);
+					
+					//West
+					var westPoint = ctx.getImageData(cellCenterX-cellWidth/2+2, cellCenterY, 1, 1).data;
+					var westColor = "#" + ("000000" + rgbToHex(westPoint[0], westPoint[1], westPoint[2])).slice(-6);
+					
+					
+					ctx.fillRect(cellCenterX, cellCenterY-cellWidth/2+2,1,1);
+					ctx.fillRect(cellCenterX, cellCenterY+cellWidth/2-1,1,1);
+					ctx.fillRect(cellCenterX+cellWidth/2-2, cellCenterY,1,1);
+					ctx.fillRect(cellCenterX-cellWidth/2+2, cellCenterY,1,1);
+					
+					if(northColor == black && eastColor == black && southColor == black && westColor == black){
+						document.getElementById(y+":"+x).value = 8;
+					}else if(northColor != black && eastColor != black && southColor != black && westColor != black){
+						document.getElementById(y+":"+x).value = 1;
+					}else{
+						if(northColor == black && southColor == black){
+							document.getElementById(y+":"+x).value = 2;
+						}else if(eastColor == black && westColor == black){
+							document.getElementById(y+":"+x).value = 3;
+						}else if(westColor == black && northColor == black){
+							document.getElementById(y+":"+x).value = 4;
+						}else if(westColor == black && southColor == black){
+							document.getElementById(y+":"+x).value = 5;
+						}else if(southColor == black && eastColor == black){
+							document.getElementById(y+":"+x).value = 6;
+						}else if(eastColor == black && northColor == black){
+							document.getElementById(y+":"+x).value = 7;
+						}else{
+							document.getElementById(y+":"+x).value = 1;
+						}
+					}
+				}else{
+					document.getElementById(y+":"+x).value = 0;
+				}
+				document.getElementById(y+":"+x).style.background="url('./res/grid_"+document.getElementById(y+":"+x).value+".png')";
+			}
+		}
+		// Underlined
+		var linePoint = ctx.getImageData(16,31, 1, 1).data;
+		var isUnderlined = ("#" + ("000000" + rgbToHex(linePoint[0], linePoint[1], linePoint[2])).slice(-6) == black);
+		document.getElementById("numberLined").checked = isUnderlined;
+		
+		// Snake Code
+		var snakeCodeX = 3;
+		var snakeCodeYtop = 328;
+		var snakeCodeYbot = 338;
+		var snakeCodeXEnd = 160;
+		
+		var snakeCode = [];
+		var possibleValues = [" ",".","|","'",":"];
+		
+		var currentSnakeX = snakeCodeX;
+		while(currentSnakeX < snakeCodeXEnd){
+			
+			var topPoint = ctx.getImageData(currentSnakeX,snakeCodeYtop, 1, 1).data;
+			var midPoint = ctx.getImageData(currentSnakeX,snakeCodeYtop + 5, 1, 1).data;
+			var botPoint = ctx.getImageData(currentSnakeX,snakeCodeYbot, 1, 1).data;
+			
+			ctx.fillRect(currentSnakeX, snakeCodeYtop,1,1);
+			ctx.fillRect(currentSnakeX, snakeCodeYtop+5,1,1);
+			ctx.fillRect(currentSnakeX, snakeCodeYbot,1,1);
+			
+			var topBlack = ("#" + ("000000" + rgbToHex(topPoint[0], topPoint[1], topPoint[2])).slice(-6) == black);
+			var midBlack = ("#" + ("000000" + rgbToHex(midPoint[0], midPoint[1], midPoint[2])).slice(-6) == black);
+			var botBlack = ("#" + ("000000" + rgbToHex(botPoint[0], botPoint[1], botPoint[2])).slice(-6) == black);
+			
+			if(!topBlack && !botBlack && !midBlack){
+				snakeCode.push(" ");
+			}else if(!topBlack && !midBlack && botBlack){
+				snakeCode.push(".");
+			}else if(topBlack && midBlack && botBlack){
+				snakeCode.push("|");
+			}else if(topBlack && !midBlack && !botBlack){
+				snakeCode.push("'");
+			}else if(topBlack && !midBlack && botBlack){
+				snakeCode.push(":");
+			}
+			currentSnakeX++;
+		}
+		
+		document.getElementById("snakeCode").value = removeCusecontiveDuplicates(snakeCode).join("");
+		
+		// Chiffres
+		var chifferCanvas = document.getElementById("chifferCanvas");
+		var chifferSubImg = ctx.getImageData(0, 0, 170, 30);
+		
+		var chifferCtx = chifferCanvas.getContext("2d");
+		chifferCtx.putImageData(chifferSubImg, 0, 0);
+		
+		var chifferImg = new Image();
+		chifferImg.id = "pic";
+		chifferImg.src = chifferCanvas.toDataURL();
+		document.getElementById("chifferImg").src = chifferImg.src;
+
+		Tesseract.recognize(chifferImg.src, {
+			lang: 'fra'
+		})
+		.then(function(result){
+			var theNumber = result.text.split("\n")[0].split(" ").join("");
+			console.log(theNumber);
+			document.getElementById("number").value = theNumber;
+		})
 	};
 	
 	fillForm(playlistInfo);
-	
-	buildGridCodeTable(playlistInfo.img.grid);
+}
+
+function removeCusecontiveDuplicates(array){
+	var ret = [array[0]];
+	for(var i=0;i<array.length;i++){
+		if(ret[ret.length-1] != array[i]){
+			ret.push(array[i]);
+		}
+	}
+	return ret;
 }
 
 function fillForm(playlistInfo){
